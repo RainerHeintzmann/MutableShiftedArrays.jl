81efad762ebf80ea54b7fc51bbdcc59b7726e4d0
RainerHeintzmann <heintzmann@gmail.com>
2025-04-16 14:07:59 +0200

    streamline Extension

--- ext/CUDASupportExt.jl
+++ ext/CUDASupportExt.jl
@@ -4,20 +4,19 @@ using Adapt
 using MutableShiftedArrays
 using Base # to allow displaying such arrays without causing the single indexing CUDA error
 MutableShiftedArrayCu{N, CD} = MutableShiftedArray{<:Any,<:Any,<:Any,<:CuArray{<:Any,N,CD}}
-MutableShiftedArraySubCu{N, CD} = Union{SubArray{<:Any, <:Any, <:MutableShiftedArrayCu{N, CD}},
-                                    Base.ReshapedArray{<:Any, <:Any, <:MutableShiftedArrayCu{N, CD}},
-                                    SubArray{<:Any, <:Any, <:Base.ReshapedArray{<:Any, <:Any, <:MutableShiftedArrayCu{N, CD}}, <:Any, <:Any}}
+MutableShiftedArrayOrWrapped = Union{MutableShiftedArray,
+                                    Base.ReshapedArray{<:Any, <:Any, <:MutableShiftedArray},
+                                    SubArray{<:Any, <:Any, <:MutableShiftedArray, <:Any, <:Any}}
 
 # lets do this for the MutableShiftedArray type
 Adapt.adapt_structure(to, x::MutableShiftedArray) = MutableShiftedArray(adapt(to, parent(x)), shifts(x), size(x); default=MutableShiftedArrays.default(x));
+# suggestions by vchuravy (https://github.com/JuliaGPU/CUDA.jl/issues/2735):
+Adapt.parent_type(::Type{MutableShiftedArray{_A,_B,_C,AA}}) where {_A,_B,_C,AA} = AA
+Adapt.unwrap_type(W::Type{<:MutableShiftedArray}) = unwrap_type(parent_type(W))
 
-function Base.Broadcast.BroadcastStyle(::Type{T})  where {N, CD, T<:MutableShiftedArrayCu{N, CD}}
-    CUDA.CuArrayStyle{N,CD}()
-end
-
-# Define the BroadcastStyle for SubArray of MutableShiftedArray with CuArray
-function Base.Broadcast.BroadcastStyle(::Type{T})  where {N, CD, T<:MutableShiftedArraySubCu{N, CD}}
-    CUDA.CuArrayStyle{N,CD}()
+# function Base.Broadcast.BroadcastStyle(::Type{T})  where {N, CD, T<:MutableShiftedArraySubCu{N, CD}}
+function Base.Broadcast.BroadcastStyle(W::Type{<:MutableShiftedArrayOrWrapped})
+    return Base.Broadcast.BroadcastStyle(unwrap_type(W))
 end
 
 function Base.show(io::IO, mm::MIME"text/plain", cs::MutableShiftedArrayCu) 
